cc5f72eb21889d98bc2d551aa611577c73a76ff5
edgar <edgar.mtz.andrade@gmail.com>
2020-11-23 16:20:13 -0600

    microservicio customer journey

---
+++ src/main/java/mx/com/nmp/mscustomerjourney/CustomerjourneyApplication.java
@@ -0,0 +1,13 @@
+package mx.com.nmp.mscustomerjourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerjourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerjourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/nmp/mscustomerjourney/CustomerjourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.nmp.mscustomerjourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerjourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
