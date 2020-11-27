b7c3ebe36acd9773904cbdd76c880cd8d71a915e
edgar <edgar.mtz.andrade@gmail.com>
2020-11-27 10:52:37 -0600

    maquetado

---
+++ src/main/java/mx/com/nmp/mscustomerjourney/model/GeoIP.java
@@ -0,0 +1,37 @@
+package mx.com.nmp.mscustomerjourney.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GeoIP {
+
+    @JsonProperty("country_code2")
+    private String countryCode;
+    @JsonProperty("region_code")
+    private String regionCode;
+    private Location location;
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getRegionCode() {
+        return regionCode;
+    }
+
+    public void setRegionCode(String regionCode) {
+        this.regionCode = regionCode;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+}
